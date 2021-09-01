ab6691eb806fd763bca52a0d8afcdb442fac6d97
alanwang0425 <will.wangdev0425@gmail.com>
2021-09-01 19:44:17 +0400

    implemented henryschein search

--- apps/scrapers/henryschein.py
+++ apps/scrapers/henryschein.py
@@ -2,12 +2,13 @@ import asyncio
 import json
 import re
 from datetime import datetime
+from typing import List
 
 from aiohttp import ClientResponse
 from scrapy import Selector
 
 from apps.scrapers.base import Scraper
-from apps.scrapers.schema import Order
+from apps.scrapers.schema import Order, Product
 from apps.types.scraper import LoginInformation
 
 HEADERS = {
@@ -28,15 +29,35 @@ HEADERS = {
     "accept-language": "en-US,en;q=0.9",
 }
 
+SEARCH_HEADERS = {
+    "authority": "www.henryschein.com",
+    "cache-control": "max-age=0",
+    "upgrade-insecure-requests": "1",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+    "AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/92.0.4515.159 Safari/537.36",
+    "accept": "text/html,application/xhtml+xml, application/xml;q=0.9, "
+    "image/avif,image/webp,image/apng, */*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "sec-ch-ua": '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-fetch-site": "none",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-user": "?1",
+    "sec-fetch-dest": "document",
+    "accept-language": "en-US,en;q=0.9",
+}
+
 
 class HenryScheinScraper(Scraper):
+    BASE_URL = "https://www.henryschein.com"
+
     async def _check_authenticated(self, response: ClientResponse) -> bool:
         res = await response.json()
         return res.get("IsAuthenticated", False)
 
     async def _get_login_data(self) -> LoginInformation:
         return {
-            "url": "https://www.henryschein.com/webservices/LoginRequestHandler.ashx",
+            "url": f"{self.BASE_URL}/webservices/LoginRequestHandler.ashx",
             "headers": HEADERS,
             "data": {
                 "username": self.username,
@@ -115,7 +136,7 @@ class HenryScheinScraper(Scraper):
         return order_product
 
     async def get_orders(self, perform_login=False):
-        url = "https://www.henryschein.com/us-en/Orders/OrderStatus.aspx"
+        url = f"{self.BASE_URL}/us-en/Orders/OrderStatus.aspx"
 
         if perform_login:
             await self.login()
@@ -128,3 +149,28 @@ class HenryScheinScraper(Scraper):
             )
             tasks = (self.get_order(order_dom) for order_dom in orders_dom)
             return await asyncio.gather(*tasks, return_exceptions=True)
+
+    async def search_products(self, query: str, page: int = 1, per_page: int = 30) -> List[Product]:
+        await self.login()
+        url = f"{self.BASE_URL}/us-en/Search.aspx"
+        params = {"searchkeyWord": query, "pagenumber": page}
+        async with self.session.get(url, headers=SEARCH_HEADERS, params=params) as resp:
+            response_dom = Selector(text=await resp.text())
+
+        products = []
+        for product_dom in response_dom.css("section.product-listing ol.products > li.product > .title"):
+            product_detail = product_dom.xpath(".//script[@type='application/ld+json']//text()").extract_first()
+            product_detail = json.loads(product_detail)
+            products.append(
+                Product(
+                    product_id=product_detail["sku"],
+                    name=product_detail["name"],
+                    description=product_detail["description"],
+                    url=product_detail["url"],
+                    image=product_detail["image"],
+                    price="",
+                    retail_price="",
+                )
+            )
+
+        return products
