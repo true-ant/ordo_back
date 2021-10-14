543890972ca06817a7211ff190402f709d418fed
alanwang0425 <will.wangdev0425@gmail.com>
2021-10-14 17:58:04 +0400

    update staging info

--- config/settings/staging.py
+++ config/settings/staging.py
@@ -11,15 +11,6 @@ ALLOWED_HOSTS = [
     "ordo-backend-dev.us-east-1.elasticbeanstalk.com",
     "api.staging.joinordo.com",
 ]
-
-CELERY_RESULT_BACKEND = None
-
-BROKER_TRANSPORT_OPTIONS = {
-    "polling_interval": 20,
-    "region": "us-east-1",
-}
-BROKER_URL = f"sqs://{AWS_ACCESS_KEY_ID}:{AWS_SECRET_ACCESS_KEY}@"  # noqa
-CELERY_BROKER_URL = BROKER_URL
 CORS_ALLOWED_ORIGINS = [
     "https://staging.joinordo.com",
     "http://localhost:8080",
