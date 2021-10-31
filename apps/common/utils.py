f8363250d08ffd0e45952b33dcefed02c421f903
alanwang0425 <will.wangdev0425@gmail.com>
2021-10-31 16:33:29 +0400

    updated similarity function

--- apps/common/utils.py
+++ apps/common/utils.py
@@ -7,37 +7,30 @@ def generate_token():
     return uuid.uuid4().hex + uuid.uuid4().hex
 
 
-def get_similarity(product1, product2, key=None):
-    if isinstance(product1, str) and isinstance(product2, str):
-        s1 = product1
-        s2 = product2
-    else:
-        s1 = getattr(product1, key)
-        s2 = getattr(product2, key)
+def get_similarity(*products, key=None):
+    total_words = set()
+    matched_words = set()
+    total_numeric_values = set()
+    matched_numeric_values = set()
 
-    s1_numeric_values = set(map(str.lower, re.findall(r"\w*[\d]+\w*", s1)))
-    s2_numeric_values = set(map(str.lower, re.findall(r"\w*[\d]+\w*", s2)))
-    s1_words = set(map(str.lower, re.findall(r"\w+", s1)))
-    s2_words = set(map(str.lower, re.findall(r"\w+", s2)))
-    # s1_numeric_values = set(map(str.lower, re.findall(r'(\w*[:,.%-/]*[\d]+[:,.%-/]*\w*[:,.%-.]*)+', s1)))
-    # s2_numeric_values = set(map(str.lower, re.findall(r'(\w*[:,.%-/]*[\d]+[:,.%-/]*\w*[:,.%-.]*)+', s2)))
-    s1_words = s1_words - s1_numeric_values
-    s2_words = s2_words - s2_numeric_values
-    total_words = len(s1_words | s2_words)
-    match_words = len(s1_words & s2_words)
-    total_numeric_values = len(s1_numeric_values | s2_numeric_values)
-    match_numeric_values = len(s1_numeric_values & s2_numeric_values)
-    percentage = 0.3 * match_words / total_words + 0.7 * match_numeric_values / total_numeric_values
+    for product_i, product in enumerate(products):
+        product_name = product if isinstance(product, str) else getattr(product, key)
+        product_numeric_values = set(map(str.lower, re.findall(r"\w*[\d]+\w*", product_name)))
+        product_words = set(map(str.lower, re.findall(r"\w+", product_name)))
+        product_words = product_words - product_numeric_values
+        total_words.update(product_words)
+        total_numeric_values.update(product_numeric_values)
+        if product_i == 0:
+            matched_words = product_words
+            matched_numeric_values = product_numeric_values
+        else:
+            matched_words &= product_words
+            matched_numeric_values &= product_numeric_values
+
+    percentage = 0.3 * len(matched_words) / len(total_words) + 0.7 * len(matched_numeric_values) / len(
+        total_numeric_values
+    )
     return percentage
-
-
-def n_similarities(*args, **kwargs):
-    base_s = args[0]
-    similarity = 1
-    key = kwargs.get("key", None)
-    for arg in args[1:]:
-        similarity *= get_similarity(base_s, arg, key)
-    return similarity
 
 
 def group_products(search_results):
@@ -47,9 +40,11 @@ def group_products(search_results):
     }
 
     vendors_search_result_products = []
+    vendors_matched_products = {}
     for search_result in search_results:
         if not isinstance(search_result, dict):
             continue
+        vendor_slug = search_result["vendor_slug"]
         meta["total_size"] += search_result["total_size"]
         meta["vendors"].append(
             {
@@ -59,18 +54,40 @@ def group_products(search_results):
             }
         )
         vendors_search_result_products.append(search_result["products"])
+        vendors_matched_products[vendor_slug] = set()
 
     meta["last_page"] = all([vendor_search["last_page"] for vendor_search in meta["vendors"]])
 
     # group by similar products
+    search_result_vendors_count = len(vendors_search_result_products)
+    mismatching_pairs = set()
+    longest_mismatching_pair_length = 2
     products = []
     similar_candidate_products = []
-    vendors_matched_products = [set() for _ in range(len(vendors_search_result_products))]
-    threshold = 0.7 ** len(vendors_search_result_products)
-    for vendor_products in itertools.product(*vendors_search_result_products):
-        similarity = n_similarities(*vendor_products, key="name")
-        if similarity > threshold:
-            similar_candidate_products.append([f"{similarity:.2f}", *vendor_products])
+    n_similarity = 2
+    while n_similarity <= search_result_vendors_count:
+        threshold = 0.6 ** (n_similarity - 1)
+        vendors_products_combinations = itertools.combinations(vendors_search_result_products, n_similarity)
+        for vendors_products_combination in vendors_products_combinations:
+            for vendor_products in itertools.product(*vendors_products_combination):
+                # when finding more than 3-length similar products we need to check that
+                # sub-set of products belongs to mismatching pairs. If mismatching set contains subset of products
+                # we don't need to calculate the similarity
+                if n_similarity > longest_mismatching_pair_length:
+                    contains_mismatching_pair = any(
+                        [set(mismatching_pair) & set(vendor_products) for mismatching_pair in mismatching_pairs]
+                    )
+                    if contains_mismatching_pair:
+                        continue
+
+                similarity = get_similarity(*vendor_products, key="name")
+                if similarity < threshold:
+                    mismatching_pairs.add(vendor_products)
+                    longest_mismatching_pair_length = len(vendor_products)
+                else:
+                    similar_candidate_products.append([f"{similarity:.2f}", *vendor_products])
+
+        n_similarity += 1
 
     # remove duplicates in candidate list
     if similar_candidate_products:
@@ -92,6 +109,7 @@ def group_products(search_results):
         for vendor_product in vendor_search_result_products:
             if vendor_product not in vendor_matched_products:
                 products.append(vendor_product.to_dict())
+
     return meta, products
 
 
@@ -132,7 +150,7 @@ if __name__ == "__main__":
     similarities = []
     products_list = [net32_products, henry_products, benco_products]
     for products in itertools.product(*products_list):
-        similarity = n_similarities(*products)
+        similarity = get_similarity(*products)
         similarities.append((f"{similarity:.2f}", *products))
 
     for i in sorted(similarities, key=lambda x: x[0], reverse=True)[:10]:
