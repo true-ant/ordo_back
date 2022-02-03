14589bf90d10d5cbdd3fb941fb7321b90bccf51e
alanwang0425 <will.wangdev0425@gmail.com>
2022-02-03 18:18:09 +0400

    fixed pending order approval acount on dashboard

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -129,7 +129,10 @@ class OrderViewSet(AsyncMixin, ModelViewSet):
             total_amount = queryset[0].month_total_amount
             average_amount = (total_amount / orders_count).quantize(Decimal(".01"), rounding=decimal.ROUND_UP)
 
-        pending_orders_count = m.Order.objects.filter(status=m.OrderStatus.WAITING_APPROVAL).count()
+        pending_orders_count = m.VendorOrder.objects.filter(
+            order__office_id=office_id,
+            status=m.OrderStatus.WAITING_APPROVAL,
+        ).count()
         vendors = (
             m.VendorOrder.objects.filter(
                 Q(order_date__gte=month_first_day)
