9f47b6c7daf09e56500a3b7507cccf6d03fd118a
alanwang0425 <will.wangdev0425@gmail.com>
2022-08-06 04:01:21 -0400

    comment header

--- apps/scrapers/implant_direct.py
+++ apps/scrapers/implant_direct.py
@@ -135,43 +135,11 @@ class ImplantDirectScraper(Scraper):
         return await self._search_products_from_table(query, page, min_price, max_price, sort_by, office_id)
 
     async def getHomePage(self):
-        headers = {
-            'authority': 'store.implantdirect.com',
-            'cache-control': 'max-age=0',
-            'sec-ch-ua': '" Not;A Brand";v="99", "Google Chrome";v="97", "Chromium";v="97"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'sec-fetch-site': 'none',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-user': '?1',
-            'sec-fetch-dest': 'document',
-            'accept-language': 'en-US,en;q=0.9,ko;q=0.8,pt;q=0.7',
-        }
-
-        response = await self.session.get('https://store.implantdirect.com/', headers=headers)
+        response = await self.session.get('https://store.implantdirect.com/', headers=HOMEPAGE_HEADERS)
         return response
 
     async def getLoginPage(self, login_link):
-        headers = {
-            'authority': 'store.implantdirect.com',
-            'sec-ch-ua': '" Not;A Brand";v="99", "Google Chrome";v="97", "Chromium";v="97"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'sec-fetch-site': 'same-origin',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-user': '?1',
-            'sec-fetch-dest': 'document',
-            'referer': 'https://store.implantdirect.com/',
-            'accept-language': 'en-US,en;q=0.9,ko;q=0.8,pt;q=0.7',
-        }
-
-        response = await self.session.get(login_link, headers=headers)
+        response = await self.session.get(login_link, headers=LOGIN_PAGE_HEADERS)
         return response
 
     async def getProductPage(self, link):
