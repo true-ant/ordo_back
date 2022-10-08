fdfc3e44881d464c49fcb390804f1e199830c9a7
alanwang0425 <will.wangdev0425@gmail.com>
2022-10-08 22:11:09 +0000

    fix:email template

--- apps/accounts/management/commands/email_test.py
+++ apps/accounts/management/commands/email_test.py
@@ -1,20 +1,29 @@
+import os
+import html
 from django.core.mail import send_mail
-from django.conf import settings
 from django.template.loader import render_to_string
 import datetime
 
+
+
+SITE_URL = "https://staging.joinordo.com"
+FROM_EMAIL = "noreply@joinordo.com"
+
 class Company():
-    name = ""
+    def __init__(self, name) -> None:
+        self.name = name
 
 class Office():
-    name=""
-    shipping_address = ""
+    def __init__(self, name, addr = ""):
+        self.name=name
+        self.shipping_address = addr
 
 class User():
-    full_name = ""
+    def __init__(self, name):
+        self.full_name = name
 def test_approval():
     # Approval Needed
-    office = Office(name="Columbine Creek Dentistry")
+    office = Office("Columbine Creek Dentistry")
     email_template = "order_approval_needed.html"
     htm_content = render_to_string(
         f"emails/{email_template}",
@@ -27,20 +36,20 @@ def test_approval():
             "total_amount": "$10000",
             "remaining_budget": 1000,
             "office": office,
-            "SITE_URL": settings.SITE_URL,
+            "SITE_URL": SITE_URL,
         },
     )
     send_mail(
         subject="Order approval needed",
         message="message",
-        from_email=settings.DEFAULT_FROM_EMAIL,
+        from_email=FROM_EMAIL,
         recipient_list=list("zachburau@gmail.com"),
         html_message=htm_content,
     )
 
 def test_confirmation():
     # Approval Needed
-    office = Office(name="Columbine Creek Dentistry")
+    office = Office("Columbine Creek Dentistry")
     email_template = "order_creation.html"
     htm_content = render_to_string(
         f"emails/{email_template}",
@@ -53,13 +62,13 @@ def test_confirmation():
             "total_amount": "$10000",
             "remaining_budget": 1000,
             "office": office,
-            "SITE_URL": settings.SITE_URL,
+            "SITE_URL": SITE_URL,
         },
     )
     send_mail(
         subject="Order Confirmation",
         message="message",
-        from_email=settings.DEFAULT_FROM_EMAIL,
+        from_email=FROM_EMAIL,
         recipient_list=list("zachburau@gmail.com"),
         html_message=htm_content,
     )
@@ -69,13 +78,13 @@ def test_reset_password():
         "emails/reset_password.html",
         {
             "TOKEN": "",
-            "SITE_URL": settings.SITE_URL,
+            "SITE_URL": SITE_URL,
         },
     )
     send_mail(
         subject="Password Reset",
         message="message",
-        from_email=settings.DEFAULT_FROM_EMAIL,
+        from_email=FROM_EMAIL,
         recipient_list="zachburau@gmail.com",
         html_message=htm_content,
     )
@@ -85,28 +94,28 @@ def test_welcome_to_ordo():
     send_mail(
         subject="Welcome to Ordo!",
         message="message",
-        from_email=settings.DEFAULT_FROM_EMAIL,
+        from_email=FROM_EMAIL,
         recipient_list="zachburau@gmail.com",
         html_message=htm_content,
     )
 
 def test_invited():
-    inviter = User(full_name="Test Inviter")
-    company = Company(name="Columbine Creek Dentistry")
+    inviter = User("Test Inviter")
+    company = Company("Columbine Creek Dentistry")
     htm_content = render_to_string(
         "emails/invite.html",
         {
             "inviter": inviter,
             "company": company,
             "TOKEN": "",
-            "SITE_URL": settings.SITE_URL,
+            "SITE_URL": SITE_URL,
         },
     )
     send_mail(
         subject="You've been invited to Join Ordo!",
         message="message",
-        from_email=settings.DEFAULT_FROM_EMAIL,
-        recipient_list=["zachburau@gmail.com"],
+        from_email=FROM_EMAIL,
+        recipient_list=["kisney.dev.11@gmail.com"],
         html_message=htm_content,
     )
     
@@ -114,21 +123,22 @@ def test_update_budget():
     htm_content = render_to_string(
         "emails/update_budget.html",
         {
-            "SITE_URL": settings.SITE_URL,
+            "SITE_URL": SITE_URL,
         },
     )
 
     send_mail(
         subject="It's time to update your budget!",
         message="message",
-        from_email=settings.DEFAULT_FROM_EMAIL,
+        from_email=FROM_EMAIL,
         recipient_list=list("zachburau@gmail.com"),
         html_message=htm_content,
     )
 if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "apps.config.settings")
     test_invited()
-    test_welcome_to_ordo()
-    test_approval()
-    test_confirmation()
-    test_reset_password()
-    test_update_budget()
+    # test_welcome_to_ordo()
+    # test_approval()
+    # test_confirmation()
+    # test_reset_password()
+    # test_update_budget()
