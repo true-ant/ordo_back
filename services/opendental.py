d7f495889ec820a61b269fc66f95c252b6d73d69
alanwang0425 <will.wangdev0425@gmail.com>
2023-01-31 02:04:10 +0600

    Add OpenDental client

---
+++ services/opendental.py
@@ -0,0 +1,14 @@
+import requests
+
+QUERY_URL = "https://api.opendental.com/api/v1/queries/ShortQuery"
+
+
+class OpenDentalClient:
+    def __init__(self, auth_header):
+        self.session = requests.Session()
+        self.session.headers = {"Authorization": auth_header}
+
+    def query(self, query, offset=None):
+        params = {"Offset": offset} if offset else None
+        resp = self.session.put(QUERY_URL, params=params, json={"SqlCommand": query})
+        return resp.json()
