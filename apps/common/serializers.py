2709a6d8cfef0c3b638b3213d6178ac3a01d744a
alanwang0425 <will.wangdev0425@gmail.com>
2023-04-27 20:37:11 +0000

    Hotfix/action based querysets and better image detection

--- apps/common/serializers.py
+++ apps/common/serializers.py
@@ -1,15 +1,16 @@
 import base64
+import io
 import uuid
 
 import six
 from django.core.files.base import ContentFile
 from django.core.validators import URLValidator
+from PIL import Image
 from rest_framework.serializers import ImageField, ValidationError
 
 
 class Base64ImageField(ImageField):
     def to_internal_value(self, data):
-
         if isinstance(data, six.string_types) and data.startswith("data:image"):
             header, data = data.split(";base64,")
 
@@ -29,9 +30,8 @@ class Base64ImageField(ImageField):
         return super(Base64ImageField, self).to_internal_value(data)
 
     def get_file_extension(self, file_name, decoded_file):
-        import imghdr
-
-        extension = imghdr.what(file_name, decoded_file)
+        image = Image.open(io.BytesIO(decoded_file))
+        extension = image.format.lower()
         extension = "jpg" if extension == "jpeg" else extension
 
         return extension
