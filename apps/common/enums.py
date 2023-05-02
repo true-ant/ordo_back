c9bf8130ed7b0f64a6dd72ccbe36574dfc0b4b51
alanwang0425 <will.wangdev0425@gmail.com>
2023-05-02 19:07:38 +0000

    fixed office products mismatching

--- apps/common/enums.py
+++ apps/common/enums.py
@@ -1,4 +1,4 @@
-from enum import Enum
+from enum import Enum, IntEnum
 
 
 class SupportedVendor(Enum):
@@ -29,3 +29,12 @@ class SupportedVendor(Enum):
     OfficeDepot = "office_depot"
     EBay = "ebay"
     Safco = "safco"
+
+
+class OnboardingStep(IntEnum):
+    ACCOUNT_SETUP = 0
+    OFFICE_DETAILS = 1
+    BILLING_INFORMATION = 2
+    OFFICE_BUDGET = 3
+    INVITE_TEAM = 4
+    LINK_VENDOR = 5
---
+++ apps/orders/management/commands/fill_office_products.py
@@ -0,0 +1,53 @@
+from django.conf import settings
+from django.core.management import BaseCommand
+from django.core.paginator import Paginator
+
+from apps.accounts.models import Office, Vendor
+from apps.orders.models import OfficeProduct, Product
+
+
+class Command(BaseCommand):
+    help = "Move products to Office Products Table"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--office",
+            type=int,
+            help="office id",
+        )
+        parser.add_argument(
+            "--vendor",
+            type=str,
+            help="vendor slug",
+        )
+
+    def handle(self, *args, **options):
+        vendor_slug = options["vendor"]
+        office_id = options["office"]
+        batch_size = 1000
+
+        # Move products from formular vendors only
+        if vendor_slug not in settings.FORMULA_VENDORS:
+            return None
+
+        if not Office.objects.filter(id=office_id).exists():
+            return None
+
+        vendor = Vendor.objects.filter(slug=vendor_slug).first()
+        if vendor is None:
+            return None
+
+        queryset = Product.objects.filter(vendor=vendor)
+        paginator = Paginator(queryset, batch_size)
+        for page_number in paginator.page_range:
+            page = paginator.page(page_number)
+            office_products = [
+                OfficeProduct(
+                    office_id=office_id,
+                    product_id=vendor_product.id,
+                    vendor=vendor,
+                    office_category=vendor_product.category,
+                )
+                for vendor_product in page.object_list
+            ]
+            OfficeProduct.objects.bulk_create(office_products)
