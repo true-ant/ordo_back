ea52628f239c090835f901cfc91c8a67a97a022c
alanwang0425 <will.wangdev0425@gmail.com>
2023-05-18 11:23:07 -0400

    fix link vendor (#30)

--- apps/scrapers/net32.py
+++ apps/scrapers/net32.py
@@ -131,7 +131,7 @@ class Net32Scraper(Scraper):
                                     "name": line_item["mpName"],
                                     "description": line_item["description"],
                                     "url": f"{self.BASE_URL}/{line_item['detailLink']}",
-                                    # "images": [{"image": f"{self.BASE_URL}/media{line_item['mediaPath']}"}],
+                                    "images": [{"image": f"{self.BASE_URL}/media{line_item['mediaPath']}"}] if 'mediaPath' in line_item else [],
                                     "category": [line_item["catName"]],
                                     "price": line_item["oliProdPrice"],
                                     "vendor": self.vendor.to_dict(),
