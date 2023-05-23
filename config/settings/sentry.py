0d66231d2e5a6494273883fa8af762c2e72fa528
alanwang0425 <will.wangdev0425@gmail.com>
2023-05-24 04:33:30 +0600

    Move sentry to separate config which is imported in prod and staging

---
+++ config/settings/sentry.py
@@ -0,0 +1,24 @@
+import os
+
+import sentry_sdk
+from sentry_sdk.integrations.celery import CeleryIntegration
+from sentry_sdk.integrations.django import DjangoIntegration
+
+from .version import VERSION
+
+SENTRY_ENVIRONMENT = os.getenv("SENTRY_ENVIRONMENT", "UNKNOWN")
+SENTRY_DSN = os.getenv("SENTRY_DSN")
+
+if SENTRY_DSN:
+    TRACES_SAMPLE_RATES = {"beanstalk": 1, "celery": 0.1}
+    DEFAULT_TRACES_SAMPLE_RATE = 0.1
+    sample_rate = TRACES_SAMPLE_RATES.get(SENTRY_ENVIRONMENT, DEFAULT_TRACES_SAMPLE_RATE)
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        integrations=[DjangoIntegration(), CeleryIntegration()],
+        environment=SENTRY_ENVIRONMENT,
+        release=VERSION,
+        traces_sample_rate=sample_rate,
+        profiles_sample_rate=sample_rate,
+        send_default_pii=True,
+    )
