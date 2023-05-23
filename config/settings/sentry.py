edda11ada4b0b6cd1a78b4bb7e1a148e789acd85
alanwang0425 <will.wangdev0425@gmail.com>
2023-05-24 04:36:24 +0600

    Separate settings for profiler

--- config/settings/sentry.py
+++ config/settings/sentry.py
@@ -11,14 +11,17 @@ SENTRY_DSN = os.getenv("SENTRY_DSN")
 
 if SENTRY_DSN:
     TRACES_SAMPLE_RATES = {"beanstalk": 1, "celery": 0.1}
+    PROFILE_SAMPLE_RATES = {"beanstalk": 0.2, "celery": 0.1}
     DEFAULT_TRACES_SAMPLE_RATE = 0.1
-    sample_rate = TRACES_SAMPLE_RATES.get(SENTRY_ENVIRONMENT, DEFAULT_TRACES_SAMPLE_RATE)
+    DEFAULT_PROFILES_SAMPLE_RATE = 0.1
+    traces_sample_rate = TRACES_SAMPLE_RATES.get(SENTRY_ENVIRONMENT, DEFAULT_TRACES_SAMPLE_RATE)
+    profiles_sample_rate = PROFILE_SAMPLE_RATES.get(SENTRY_ENVIRONMENT, DEFAULT_PROFILES_SAMPLE_RATE)
     sentry_sdk.init(
         dsn=SENTRY_DSN,
         integrations=[DjangoIntegration(), CeleryIntegration()],
         environment=SENTRY_ENVIRONMENT,
         release=VERSION,
-        traces_sample_rate=sample_rate,
-        profiles_sample_rate=sample_rate,
+        traces_sample_rate=traces_sample_rate,
+        profiles_sample_rate=profiles_sample_rate,
         send_default_pii=True,
     )
