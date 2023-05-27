4f72fe5221c691641133e3873f10a94110635f8b
alanwang0425 <will.wangdev0425@gmail.com>
2023-05-27 06:50:25 +0600

    Traces sample rates

--- config/settings/sentry.py
+++ config/settings/sentry.py
@@ -11,8 +11,8 @@ SENTRY_ENVIRONMENT = os.getenv("SENTRY_ENVIRONMENT", "UNKNOWN")
 SENTRY_DSN = os.getenv("SENTRY_DSN")
 
 if SENTRY_DSN:
-    TRACES_SAMPLE_RATES = {"beanstalk": 0.5, "celery": 0.1}
-    PROFILE_SAMPLE_RATES = {"beanstalk": 0.2, "celery": 0.1}
+    TRACES_SAMPLE_RATES = {"beanstalk": 0.5, "beanstalk311": 0.5, "celery": 0.1}
+    PROFILE_SAMPLE_RATES = {"beanstalk": 0.2, "beanstalk311": 0.5, "celery": 0.1}
     DEFAULT_TRACES_SAMPLE_RATE = 0.1
     DEFAULT_PROFILES_SAMPLE_RATE = 0.1
     traces_sample_rate = TRACES_SAMPLE_RATES.get(SENTRY_ENVIRONMENT, DEFAULT_TRACES_SAMPLE_RATE)
