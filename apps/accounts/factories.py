0f1f535811b77c6197ac6b83fff0187f044208af
alanwang0425 <will.wangdev0425@gmail.com>
2023-06-01 02:32:59 +0600

    Add budget output validation in pydantic models

--- apps/accounts/factories.py
+++ apps/accounts/factories.py
@@ -1,6 +1,10 @@
+import random
+
 import factory
+from django.utils import timezone
 from factory.django import DjangoModelFactory
 
+from ..common.month import Month
 from . import models as m
 
 
@@ -41,6 +45,41 @@ class OfficeVendorFactory(DjangoModelFactory):
 
     vendor = factory.SubFactory(VendorFactory)
     office = factory.SubFactory(OfficeFactory)
+
+
+def get_current_month():
+    current_time = timezone.now()
+    month = Month(year=current_time.year, month=current_time.month)
+    return month
+
+
+class OfficeBudgetFactory(DjangoModelFactory):
+    class Meta:
+        model = m.OfficeBudget
+        exclude = ["budget_type"]
+
+    budget_type = factory.LazyFunction(lambda: random.choice(m.OfficeBudget.BudgetType.choices)[0])
+
+    dental_budget_type = factory.LazyAttribute(lambda o: o.budget_type)
+    dental_total_budget = factory.Faker("pydecimal", min_value=20000, max_value=100000)
+    dental_percentage = factory.Faker("pydecimal", min_value=3, max_value=10)
+
+    dental_budget = factory.LazyAttribute(lambda o: o.dental_total_budget * o.dental_percentage / 100)
+    dental_spend = factory.Faker("pydecimal", min_value=30, max_value=120)
+
+    adjusted_production = factory.Faker("pydecimal", min_value=30, max_value=120)
+    collection = factory.Faker("pydecimal", min_value=30, max_value=120)
+
+    # office_* is used for managing budgets for amazon
+    office_budget_type = factory.LazyAttribute(lambda o: o.budget_type)
+    office_total_budget = factory.Faker("pydecimal", min_value=20000, max_value=100000)
+    office_percentage = factory.Faker("pydecimal", min_value=3, max_value=10)
+    office_budget = factory.LazyAttribute(lambda o: o.office_total_budget * o.office_percentage / 100)
+    office_spend = factory.Faker("pydecimal", min_value=30, max_value=120)
+
+    miscellaneous_spend = factory.Faker("pydecimal", min_value=0, max_value=1000)
+
+    month = factory.LazyFunction(get_current_month)
 
 
 class CompanyMemberFactory(DjangoModelFactory):
--- apps/accounts/tests/test_company_member.py
+++ apps/accounts/tests/test_company_member.py
@@ -1,4 +1,10 @@
+import datetime
+import decimal
+import math
+from typing import Literal
+
 from faker import Faker
+from pydantic import BaseModel, root_validator
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APIClient, APITestCase
@@ -8,10 +14,46 @@ from apps.accounts.factories import (
     CompanyMemberFactory,
     OfficeFactory,
     UserFactory,
+    OfficeBudgetFactory,
 )
 from apps.accounts.models import User
+from apps.accounts.tests.factories import AdminUserFactory
 
 fake = Faker()
+
+
+class RemainingBudget(BaseModel):
+    dental: float
+    office: float
+
+
+BudgetType = Literal["production", "collection"]
+
+class BudgetOutput(BaseModel):
+    id: int
+    office: int
+    remaining_budget: RemainingBudget
+    dental_budget_type: BudgetType
+    dental_total_budget: decimal.Decimal
+    dental_percentage: decimal.Decimal
+    dental_budget: decimal.Decimal
+    dental_spend: decimal.Decimal
+    office_budget_type: BudgetType
+    office_total_budget: decimal.Decimal
+    office_percentage: decimal.Decimal
+    office_budget: decimal.Decimal
+    office_spend: decimal.Decimal
+    adjusted_production: decimal.Decimal
+    collection: decimal.Decimal
+    miscellaneous_spend: decimal.Decimal
+    month: datetime.date
+
+    @root_validator()
+    def check_remaning(cls, values):
+        rb: RemainingBudget = values["remaining_budget"]
+        assert math.isclose(rb.dental, values["dental_budget"] - values["dental_spend"])
+        assert math.isclose(rb.office, values["office_budget"] - values["office_spend"])
+        return values
 
 
 class TestUserSignUpTestCase(APITestCase):
@@ -32,3 +74,27 @@ class TestUserSignUpTestCase(APITestCase):
         )
         assert resp.status_code == status.HTTP_201_CREATED
 
+
+
+
+
+class ProductManagementTestCase(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.company = CompanyFactory()
+        cls.office = OfficeFactory(company=cls.company)
+        cls.company_member = CompanyMemberFactory(company=cls.company, office=cls.office)
+        cls.office_budget = OfficeBudgetFactory(office=cls.office)
+        cls.admin = AdminUserFactory()
+        cls.api_client = APIClient()
+        cls.api_client.force_authenticate(cls.admin)
+
+    def test_company_member(self):
+        url = reverse("members-list", kwargs={"company_pk": self.company.pk})
+        resp = self.api_client.get(url)
+        assert resp.status_code == status.HTTP_200_OK
+        data = resp.json()
+        member_data = data["data"][0]
+        office_data = member_data["office"]
+        budget = BudgetOutput(**office_data["budget"])
+        assert budget
